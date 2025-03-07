4048d66cc5089164c175f64d45e82a81d8aa43f9
Moritz <mosum@google.com>
2025-03-07 11:30:33 +0100

    add icons

--- dev/icon_treeshaker/example_flutter/lib/main.dart
+++ dev/icon_treeshaker/example_flutter/lib/main.dart
@@ -10,7 +10,13 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Icon(Icons.abc_outlined))),
+      home: Scaffold(body: Center(child: Icon(TwoIcons.acUnit))),
     );
   }
+}
+
+@staticIconProvider
+sealed class TwoIcons {
+  static const IconData acUnit = IconData(0xe800, fontFamily: 'TwoIcons');
+  static const IconData whatshot = IconData(0xe801, fontFamily: 'TwoIcons');
 }
